f9a0e9760dab68cee025801c6135074f777b6a45
silverhyuk <silverhyuk8@naver.com>
2017-12-09 23:32:08 +0900

    first commit

---
+++ application/core/MY_Loader.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: idea
+ * Date: 2017-12-09
+ * Time: 오후 10:40
+ */
+
+class MY_Loader extends CI_Loader{
+    public function template($template_name, $vars=array(), $return=FALSE){
+        $layout_header = isset($vars['layout_header']) ? $vars['layout_header'] : 'layout/layout_header';
+        $layout_side_menu = isset($vars['layout_side_menu']) ? $vars['layout_side_menu'] : 'layout/layout_side_menu';
+        $layout_footer = isset($vars['layout_footer']) ? $vars['layout_footer'] : 'layout/layout_footer';
+
+
+        $param['layout_header'] = $layout_header;
+        $param['layout_side_menu'] = $layout_side_menu;
+        $param['layout_footer'] = $layout_footer;
+        $param['template_name'] = $template_name;
+        $param['vars'] = $vars;
+
+        $this->view('layout/layout_body', $param);
+    }
+}
---
+++ application/views/layout/layout_side_menu.php
@@ -0,0 +1,82 @@
+<aside class="main-sidebar">
+    <!-- sidebar: style can be found in sidebar.less -->
+    <section class="sidebar">
+
+
+        <!-- sidebar menu: : style can be found in sidebar.less -->
+        <ul class="sidebar-menu" data-widget="tree">
+            <li class="header">MAIN NAVIGATION</li>
+            <li class="treeview">
+                <a href="#">
+                    <i class="fa fa-dashboard"></i> <span>Dashboard</span>
+                    <span class="pull-right-container">
+              <i class="fa fa-angle-left pull-right"></i>
+            </span>
+                </a>
+                <ul class="treeview-menu">
+                    <li><a href="../../index.html"><i class="fa fa-circle-o"></i> Dashboard v1</a></li>
+                    <li><a href="../../index2.html"><i class="fa fa-circle-o"></i> Dashboard v2</a></li>
+                </ul>
+            </li>
+
+            <li class="treeview active">
+                <a href="#">
+                    <i class="fa fa-folder"></i> <span>Examples</span>
+                    <span class="pull-right-container">
+              <i class="fa fa-angle-left pull-right"></i>
+            </span>
+                </a>
+                <ul class="treeview-menu">
+                    <li><a href="invoice.html"><i class="fa fa-circle-o"></i> Invoice</a></li>
+                    <li><a href="profile.html"><i class="fa fa-circle-o"></i> Profile</a></li>
+                    <li><a href="login.html"><i class="fa fa-circle-o"></i> Login</a></li>
+                    <li><a href="register.html"><i class="fa fa-circle-o"></i> Register</a></li>
+                    <li><a href="lockscreen.html"><i class="fa fa-circle-o"></i> Lockscreen</a></li>
+                    <li><a href="404.html"><i class="fa fa-circle-o"></i> 404 Error</a></li>
+                    <li><a href="500.html"><i class="fa fa-circle-o"></i> 500 Error</a></li>
+                    <li class="active"><a href="blank.html"><i class="fa fa-circle-o"></i> Blank Page</a></li>
+                    <li><a href="pace.html"><i class="fa fa-circle-o"></i> Pace Page</a></li>
+                </ul>
+            </li>
+            <li class="treeview">
+                <a href="#">
+                    <i class="fa fa-share"></i> <span>Multilevel</span>
+                    <span class="pull-right-container">
+              <i class="fa fa-angle-left pull-right"></i>
+            </span>
+                </a>
+                <ul class="treeview-menu">
+                    <li><a href="#"><i class="fa fa-circle-o"></i> Level One</a></li>
+                    <li class="treeview">
+                        <a href="#"><i class="fa fa-circle-o"></i> Level One
+                            <span class="pull-right-container">
+                  <i class="fa fa-angle-left pull-right"></i>
+                </span>
+                        </a>
+                        <ul class="treeview-menu">
+                            <li><a href="#"><i class="fa fa-circle-o"></i> Level Two</a></li>
+                            <li class="treeview">
+                                <a href="#"><i class="fa fa-circle-o"></i> Level Two
+                                    <span class="pull-right-container">
+                      <i class="fa fa-angle-left pull-right"></i>
+                    </span>
+                                </a>
+                                <ul class="treeview-menu">
+                                    <li><a href="#"><i class="fa fa-circle-o"></i> Level Three</a></li>
+                                    <li><a href="#"><i class="fa fa-circle-o"></i> Level Three</a></li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </li>
+                    <li><a href="#"><i class="fa fa-circle-o"></i> Level One</a></li>
+                </ul>
+            </li>
+            <li><a href="https://adminlte.io/docs"><i class="fa fa-book"></i> <span>Documentation</span></a></li>
+            <li class="header">LABELS</li>
+            <li><a href="#"><i class="fa fa-circle-o text-red"></i> <span>Important</span></a></li>
+            <li><a href="#"><i class="fa fa-circle-o text-yellow"></i> <span>Warning</span></a></li>
+            <li><a href="#"><i class="fa fa-circle-o text-aqua"></i> <span>Information</span></a></li>
+        </ul>
+    </section>
+    <!-- /.sidebar -->
+</aside>
