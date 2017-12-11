5aa3a78f115fbefd61f41c619fdfacc71e0169c2
ehchoi <silverhyuk8@naver.com>
2017-12-11 14:01:38 +0900

    샘플페이지

--- application/views/board/list_v.php
+++ application/views/board/list_v.php
@@ -29,7 +29,7 @@
         <div class="col-xs-12">
             <div class="box">
                 <div class="box-header">
-                    <h3 class="box-title">Responsive Hover Table</h3>
+                    <h3 class="box-title">Table</h3>
 
                     <div class="box-tools">
                         <div class="input-group input-group-sm" style="width: 150px;">
