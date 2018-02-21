07f5e6838809f2dc72734ea0514d8f4f4ae8b69c
ehchoi <silverhyuk8@naver.com>
2018-02-21 12:48:45 +0900

    사용자 수정

--- application/views/layout/layout_header.php
+++ application/views/layout/layout_header.php
@@ -2,9 +2,9 @@
     <!-- Logo -->
     <a href="<?=site_url('/');?>" class="logo">
         <!-- mini logo for sidebar mini 50x50 pixels -->
-        <span class="logo-mini"><b>SID</b></span>
+        <span class="logo-mini"><b>뭐</b></span>
         <!-- logo for regular state and mobile devices -->
-        <span class="logo-lg"><b>ServiceInfra</b>DEV</span>
+        <span class="logo-lg"><b>오늘</b>뭐먹지</span>
     </a>
     <!-- Header Navbar: style can be found in header.less -->
     <nav class="navbar navbar-static-top">
